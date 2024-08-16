4c1ae7a978f4abfe706e5b306845a3b271b4dec1
Kyan Ngana <kyanngana@gmail.com>
2024-08-16 17:51:51 -0400

    created code skeleton

---
+++ code_skeleton.py
@@ -0,0 +1,25 @@
+
+class Tictactoe:
+
+    def createBoard():
+        #todo: create a 3 by 3 board
+        pass
+
+    def createPlayers():
+        #todo: create player 1 and player 2
+        pass
+
+    def getWinner():
+        #todo write win conditions
+        '''if player one marks 3 in a row, player one wins, end game
+        if player two marks 3 in a row, player 2 wins, end game
+        else if no player marks 3 in a row, it is a tie, end game'''
+        pass
+
+    def resetButton():
+        #todo: create a reset button that clears the board.
+        pass
+
+    def runGame():
+        #todo: write code that starts the game.
+        pass
